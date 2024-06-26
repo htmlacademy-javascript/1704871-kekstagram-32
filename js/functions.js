ee568ba2e00ff1efa4f2adfc2d016bbcddeedfec
dmoldd <oldtime_tula@mail.ru>
2024-06-26 13:15:17 +0300

    пишет функцию проверки на палиндром через цикл

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,40 @@
-const checkStrokeLength = (stroke, count) => stroke.length > count || stroke.length === count;
+let l = console.log();
 
-checkStrokeLength('я помню чудное мгновенье', 20);
+/**
+ * @description функция, которая проверяет длину строки.
+ * Если длина больше или равна значению count,
+ * то функция возвращает true. В остальных случаях - false.
+ * @param {string} stroke - значение исходной строки
+ * @param {number} count - проверямая длина строки
+ */
 
-const isPalindrome = (stroke) => stroke.replaceAll(' ', '').split('').reverse().join('').toLowerCase() === stroke.toLowerCase();
+const checkStrokeLength = (stroke, count) => stroke.length >= count;
 
-isPalindrome('довод1');
+console.log(checkStrokeLength('я помню чудное мгновенье', 20));
+
+/**
+ * @description функция проверки строки на палиндром
+ * @param {string} stroke - значение исходной строки
+ */
+
+const isPalindrome = (stroke) => {
+  stroke = stroke.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i <= stroke.length - 1 / 2; i++) {
+    console.log('сравниваю букву ' + stroke[i] + ' с буквой ' + stroke[stroke.length - i - 1])
+    if (stroke[i] != stroke[stroke.length - i - 1]) {
+      return false;
+    }
+  };
+  return true;
+};
+
+console.log(isPalindrome('Ле2ша на полке клопа нашел'));
+
+/**
+ * @description функция возвращает целое число из строки
+ * @param {string} stroke - значение исходной строки
+ */
 
 const convertToInteger = (stroke) => Math.round(Number(stroke.replace(/[^+\d]/g, '')));
 
-convertToInteger('агент 007');
-
+console.log(convertToInteger('агент 007'));
