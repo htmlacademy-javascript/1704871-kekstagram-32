f2a5a896a6646bf806c8e7f35e1207093c49d8c5
Dmoldd <oldtime_tula@mail.ru>
2024-07-13 00:43:28 +0300

    начинает выполнять задание Отрисуй меня полностью

---
+++ js/render-miniature.js
@@ -0,0 +1,13 @@
+// import {generatePosts} from './data.js';
+
+// const template = document.getElementById('picture');
+
+// const createMiniature = () => {
+//   const templateClone = template.content.cloneNode('true');
+//   templateClone.url =
+// };
+
+// const createMiniatures = () => {};
+
+// console.log(template);
+// console.log(templateClone);
